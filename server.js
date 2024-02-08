e17c93c92d327df8dc6545c681953ce0a3e444f7
RubenErhardt <rubengamebattles@outlook.com>
2024-02-08 23:24:07 +0100

    push

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import fetchJson from './helpers/fetch-json.js'
 // Haal data op uit de FDND API, ga pas verder als de data gedownload is
 const data = await fetchJson('https://fdnd.directus.app/items/person/16')
 data.data.custom = JSON.parse(data.data.custom)
-console.log(data);
+console.log(data.data.custom.bio2);
 
 // Maak een nieuwe express app aan
 const app = express()
